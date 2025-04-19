1dd03d3ebf09c4d253bee4613cd48db7f4325377
NamlessChokko <osmaoficial0@gmail.com>
2025-04-19 17:23:56 -0400

    Enhance Sentry integration: add browser tracing and update initialization comments

--- src/instrument.js
+++ src/instrument.js
@@ -1,6 +1,12 @@
-// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 const Sentry = require("@sentry/node");
 
+// Ensure to call this before requiring any other modules!
 Sentry.init({
     dsn: "https://b4f7e39e525cdc61e74146d36e62f5f6@o4509181755260928.ingest.us.sentry.io/4509181755523072",
+    integrations: [Sentry.browserTracingIntegration()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 });
